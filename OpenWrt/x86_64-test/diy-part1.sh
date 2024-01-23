6e30f9fe6d12425be84f6df5af4e3a917d4f433c
Lorchr <lorchr@163.com>
2024-01-23 09:37:44 +0800

    update

--- OpenWrt/x86_64-test/diy-part1.sh
+++ OpenWrt/x86_64-test/diy-part1.sh
@@ -14,12 +14,12 @@
 # sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-# sed -i '$a src-git opp https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-# sed -i '$a src-git sml https://github.com/kenzok8/small' feeds.conf.default
+sed -i '$a src-git opp https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git sml https://github.com/kenzok8/small' feeds.conf.default
 # sed -i '$a src-git pkgs https://github.com/ysx88/openwrt-packages' feeds.conf.default
 # sed -i '$a src-git xiaorouji https://github.com/xiaorouji/openwrt-passwall-packages' >> feeds.conf.default
 # sed -i '$a src-git haiibo https://github.com/haiibo/openwrt-packages' feeds.conf.default
-sed -i '$a src-git kiddin9 https://github.com/kiddin9/openwrt-packages' feeds.conf.default
+# sed -i '$a src-git kiddin9 https://github.com/kiddin9/openwrt-packages' feeds.conf.default
 
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >> feeds.conf.default
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >> feeds.conf.default
